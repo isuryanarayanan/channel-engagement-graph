46a3be0eb5f9a83a44faf0c0f57fd1de9cc18cfb
isuryanarayanan <devdoodan@gmail.com>
2023-06-11 13:03:22 +0530

    deployments

--- src/helpers/engagementHelper.tsx
+++ src/helpers/engagementHelper.tsx
@@ -3,6 +3,7 @@ import Highcharts from "highcharts";
 const engagementMessageOverTimeChartOptions = (messageCountList: any, channels:any): Highcharts.Options => {
     console.log("messageCountList", messageCountList)
     console.log("channels", channels)
+
     return  {
         title: {
             text: 'My chart'
