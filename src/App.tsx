a867fb2f1a59bf020b38847e61b7ee3ede50308f
isuryanarayanan <devdoodan@gmail.com>
2023-06-11 16:35:34 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ const EngagementMessagesOverTime = ()=>{
   />
 }
 
-
 function App() {
 
   return (
--- src/helpers/engagementHelper.tsx
+++ src/helpers/engagementHelper.tsx
@@ -1,20 +1,48 @@
 import Highcharts from "highcharts";
 
-const engagementMessageOverTimeChartOptions = (messageCountList: any, channels:any): Highcharts.Options => {
-    console.log("messageCountList", messageCountList)
-    console.log("channels", channels)
+const engagementMessageOverTimeChartOptions = (
+  messageCountList: any,
+  channels: any
+): Highcharts.Options => {
+  const generalChannelId = "825030076239577109";
 
-    return  {
-        title: {
-            text: 'My chart'
-        },
-        series: [{
-            type: 'line',
-            data: [1, 2, 3]
-        }]
-    };
-}
-   
+  const generalChannelMessages = messageCountList.filter(
+    (message: { channelId: string; }) => message.channelId === generalChannelId
+  );
+
+  const data = generalChannelMessages.map((message: { timeBucket: string | number | Date; count: string; }) => ({
+    x: new Date(message.timeBucket).getTime(),
+    y: parseInt(message.count),
+  }));
+
+  const channelName = channels.find((channel: { value: string; }) => channel.value === generalChannelId)?.name;
+
+
+  return {
+    title: {
+      text: "Discord server statistics",
+    },
+    xAxis: {
+      type: "datetime",
+      title: {
+        text: "Date",
+      },
+    },
+    yAxis: {
+      title: {
+        text: "Number of Messages",
+      },
+    },
+    series: [
+      {
+        name: channelName,
+        type: "spline",
+        data: data,
+      },
+    ],
+  };
+};
+
 export default {
-    engagementMessageOverTimeChartOptions
-}
+  engagementMessageOverTimeChartOptions,
+};
